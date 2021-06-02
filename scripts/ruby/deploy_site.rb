96101a057a1d61797dec85ca47b9d89d342882ea
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-02 17:07:27 +0530

    deploy script performs template evaluation as well, add projects template and html file, updated index.html file with new template, better output in scp transfer

--- scripts/ruby/deploy_site.rb
+++ scripts/ruby/deploy_site.rb
@@ -1,17 +1,36 @@
-# Generate the templates into HTML files
-
-
-# scp files from local to remote system
 require 'net/scp'
 
+# Generate the templates into HTML files
+puts "Generating the static files from templates..."
+`ruby scripts/ruby/generate_post_link.rb projects/ templates/projects.html.erb projects.html`
+`ruby scripts/ruby/generate_post_link.rb posts/ templates/index.html.erb index.html`
+puts "Static file generation complete..."
+
+puts ""
+
+# scp files from local to remote system
+puts "Copying files to the webserver"
 fh = File.open(".scpconfig")
+
+transfer = {}
 
 Net::SCP.start("a93", "abhishek") do |scp|
   fh.each_line do |line|
     puts line
     path = line.split
     scp.upload(path[0], path[1], :recursive => true) do |ch, name, sent, total|
-      puts "#{name}: #{sent}/#{total}"
+      transfer[name] = "#{sent}/#{total}"
+      print "#{name}: #{sent}/#{total}                 "
+      STDOUT.goto_column(0)
     end
   end
+end
+
+STDOUT.erase_line(2)
+puts "" 
+
+puts "Transfer complete"
+puts "Transfer status by file..."
+transfer.each do |k, v|
+  puts "#{k}\t\t\t=> #{v}"
 end
