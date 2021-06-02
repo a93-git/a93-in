393c1553201060863af374bd8d53dec9f3b0feb0
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-02 13:25:42 +0530

    Generate index.html page using template, dynamically update the posts

--- generate_post_link.rb
+++ generate_post_link.rb
@@ -1,4 +1,5 @@
 require 'nokogiri'
+require 'erb'
 
 if (ARGV[0] == nil)
   puts "Filepath is required. No filepath provided. Exiting"
@@ -7,7 +8,7 @@ else
   PATH = ARGV[0].chomp
 end
 
-links = []
+post_links = []
 
 if Dir.open(PATH)
   files = Dir.open(PATH).children
@@ -20,27 +21,32 @@ if Dir.open(PATH)
     post_title = h1_tag[0].to_s.sub("<h1>", "").sub("</h1>", "").strip
     # Create the html to insert
     link = "<p><a href=#{path}>#{post_title}</a></p>"
-    links << link
+    post_links << link
   end
 end
 
 puts "Looking for index.html in current directory"
 index_file = Dir.glob("index.html")[0]
 if index_file != nil 
-  puts "Found index.html"
+  puts "Found index.html in the current directory"
   puts index_file
 else
   puts "Couldn't find index.html. Please spcify a path: "
   index_file = gets.chomp
 end
 
-puts links
-
 # Write the data to a separate html file
 File.open("post_links.html", File::RDWR|File::CREAT, 0644) do |fh|
-  links.each do |l|
+  post_links.each do |link|
     # Get an exclusive lock to the current file
     fh.flock(File::LOCK_EX)
-    fh.write("#{l}\n")
+    fh.write("#{link}\n")
   end
+end
+
+fh = File.open("index.html.erb")
+template = ERB.new(fh.read)
+
+File.open("index.html", File::RDWR|File::CREAT, 0644) do |fh|
+  fh.write(template.result)
 end
