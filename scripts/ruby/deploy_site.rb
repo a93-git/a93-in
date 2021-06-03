f4dfbdcf5bfd460c2db3430db7636e8e2a3af158
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-04 00:30:24 +0530

    Newly created files have mode 0644, generate a list of markdown files to be parsed, remove the redundant paramter to the GenerateFileTree constructor

--- scripts/ruby/deploy_site.rb
+++ scripts/ruby/deploy_site.rb
@@ -22,34 +22,42 @@ puts "Static file generation complete..."
 
 puts ""
 
-# Generating HTML from markdown files
-puts "Generating HTML from markdown files..."
+# Get paths to all the markdown files
+generator_markdown = GenerateFileTree.new([".md$"])
 markdown_paths.each do |path|
-  dir = Dir.open(path).children # Array of all the files in the path
-
-  dir.each do |file|
-    # Only parse the files that end with .md
-    unless /\w*.md/.match(file) == nil
-      ht = ParseMarkdown.get_parser.render(File.open("#{path}/#{file}").read) 
-      filename = "#{path}/#{file.split(".")[0]}.html" 
-
-      puts "Parsing #{file}"
-      # providing a code block ensures that we are automatically closing the 
-      # opened file. So, no need to close explicitly. 
-      # File::CREAT -> create the file if doesn't exist
-      # File::WRONLY -> open the file in write-only mode
-      # See man page for open for the modes and Ruby's doc for File.open at 
-      # https://ruby-doc.org/core-2.5.0/File.html#method-c-open
-      # 00744 -> owner can rwx, others can read
-      File.open(filename, File::CREAT | File::WRONLY, 00744) do |fh|
-        fh.write(ht)
-      end
-    end
+  # We are getting the list of files eligible for transfer
+  begin
+    generator_markdown.rec_listing!(path)
+  rescue NoMethodError => e
+    puts "Skipping #{path[0]} as it couldn't be parsed", ""
+  rescue Exception => e
+    puts e.message, e.class, ""
   end
 end
+markdown_files = generator_markdown.file_list
+puts markdown_files
+
+# Generating HTML from markdown files
+puts "Generating HTML from markdown files..."
+markdown_files.each do |file|
+  ht = ParseMarkdown.get_parser.render(File.open("#{file}").read) 
+  filename = "#{file.split(".")[0]}.html" 
+  puts "Parsing #{file}"
+  # providing a code block ensures that we are automatically closing the 
+  # opened file. So, no need to close explicitly. 
+  # File::CREAT -> create the file if doesn't exist
+  # File::WRONLY -> open the file in write-only mode
+  # See man page for open for the modes and Ruby's doc for File.open at 
+  # https://ruby-doc.org/core-2.5.0/File.html#method-c-open
+  # 00744 -> owner can rwx, others can read
+  File.open(filename, File::CREAT | File::WRONLY, 00644) do |fh|
+    fh.write(ht)
+  end
+end
+
 puts "Done generating HTML from markdown files...", ""
 
-generator = GenerateFileTree.new("", scp_exclude_paths)
+generator = GenerateFileTree.new(scp_exclude_paths)
 
 # For each path and destination line in the configfile
 scp_paths.each do |path|
@@ -57,7 +65,7 @@ scp_paths.each do |path|
   begin
     generator.rec_listing(path)
   rescue NoMethodError => e
-    puts "Skipping #{path[0]} as it couldn't be parsed", ""
+    puts "Skipping #{path} as it couldn't be parsed", ""
   rescue Exception => e
     puts e.message, e.class, ""
   end
@@ -84,7 +92,7 @@ Net::SSH.start("a93", "abhishek") do |ssh|
   puts "Current files at webserver root path on remote server"
   output = ssh.exec!("ls -la #{webserver_path}")
   puts output, ""
-  
+
   puts "Checking for remote folders..."
   checked = []
   # A path once checked won't be checked again
