1420e534a266e51c3f9c18b46e39f9e59bca1008
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-04 00:26:52 +0530

    Add bang version of rec_listing to exclude everything that doesn't match the exclude filter list

--- scripts/ruby/generate_file_tree.rb
+++ scripts/ruby/generate_file_tree.rb
@@ -9,7 +9,7 @@ class GenerateFileTree
 
   attr_reader :file_list, :exclude_paths
 
-  def initialize(path, exclude_paths)
+  def initialize(exclude_paths)
     @exclude_paths = exclude_paths
     @file_list = []
   end
@@ -25,7 +25,6 @@ class GenerateFileTree
   end
 
   def rec_listing(local_path="")
-
     # Go through each entity in current directory
     Dir.open(local_path).children.each do |child|
       filepath = "#{local_path}/#{child}"
@@ -49,5 +48,30 @@ class GenerateFileTree
     @file_list
   end
 
+  def rec_listing!(local_path="")
+    # Go through each entity in current directory
+    Dir.open(local_path).children.each do |child|
+      filepath = "#{local_path}/#{child}"
+      rec_listing!(filepath) if File.directory?(filepath)
+      @file_list << filepath if (!File.directory?(filepath) and exclude?(filepath))
+    end
+    @file_list
+  rescue  Errno::ENOTDIR => e
+    puts "#{local_path} is not a directory. Returning the same path", ""
+    # If the given path is not a directory but a file, append it to the list of 
+    # files provided
+    @file_list << local_path if exclude?(local_path)
+    @file_list
+  rescue Errno::ENOENT => e
+    puts e.class, e.message, "Path \"#{local_path}\" doesn't exist", ""
+  rescue Exception => e
+    puts e.class, e.message, e.backtrace, ""
+    raise
+  ensure
+    # Ensure that the return value is atleast the same list that came in
+    @file_list
+  end
+
   private :exclude?
 end
+
