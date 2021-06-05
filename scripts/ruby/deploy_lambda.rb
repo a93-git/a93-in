1d844e2a595fec66716d7945c9cb685447462038
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-06 03:12:31 +0530

    Refactor the create function code into a class, add method to invoke the function, constructor takes zip_filename as the first argument and the list of files to zip as second onwards

--- scripts/ruby/deploy_lambda.rb
+++ scripts/ruby/deploy_lambda.rb
@@ -1,43 +1,61 @@
 require 'aws-sdk-lambda'
 require './zip_files'
 
-zipper = ZipFilesRec.new(*ARGV[0..-2], ARGV[-1])
-zipper.zip
-zip_filename = zipper.zip_filename
 
-Aws.config.update(
-  region: 'us-east-1',
-  credentials: Aws::InstanceProfileCredentials.new
-)
+class LambdaHandler
+  def initialize(zip_filename="", *files_to_zip)
+    @files_to_zip = files_to_zip
+    @zip_filename = zip_filename
+    Aws.config.update(
+      region: 'us-east-1',
+      credentials: Aws::InstanceProfileCredentials.new
+    )
 
-lambda_client = Aws::Lambda::Client.new
+    @lambda_client = Aws::Lambda::Client.new
+  end
 
-begin
-  response = lambda_client.create_function({
-    function_name: "a93_message_handler", 
-    runtime: "ruby2.7", 
-    role: "arn:aws:iam::979558485280:role/a93_messaging_lambda_role", 
-    handler: "message_handling_lambda.handler",
-    code: { 
-      zip_file: File.open("#{zip_filename}", "rb"),
-    },
-    description: "Sends SNS notification when a new message ends up in DynamoDB",
-    timeout: 30,
-    memory_size: 128,
-    publish: true,
-    package_type: "Zip"
-  })
-rescue Aws::Lambda::Errors::ResourceConflictException => e
-  puts "Error in creating the Lambda function"
-  puts e.message, e.class
-rescue Aws::Lambda::Errors::AccessDeniedException => e
-  puts "Access denied error while creating the Lambda function"
-  puts e.message, e.class
-rescue Exception => e
-  puts "Error occured while creating the Lambda function"
-  puts e.message, e.class
-ensure 
-  response
+  def deploy_lambda
+    zipper = ZipFilesRec.new(files_to_zip, zip_filename)
+    zipper.zip
+    zip_filename = zipper.zip_filename
+
+    response = @lambda_client.create_function({
+      function_name: "a93_message_handler", 
+      runtime: "ruby2.7", 
+      role: "arn:aws:iam::979558485280:role/a93_messaging_lambda_role", 
+      handler: "message_handling_lambda.handler",
+      code: { 
+        zip_file: File.open("#{zip_filename}", "rb"),
+      },
+      description: "Sends SNS notification when a new message ends up in DynamoDB",
+      timeout: 30,
+      memory_size: 128,
+      publish: true,
+      package_type: "Zip"
+    })
+  rescue Aws::Lambda::Errors::ResourceConflictException => e
+    puts "Error in creating the Lambda function"
+    puts e.message, e.class
+  rescue Aws::Lambda::Errors::AccessDeniedException => e
+    puts "Access denied error while creating the Lambda function"
+    puts e.message, e.class
+  rescue Exception => e
+    puts "Error occured while creating the Lambda function"
+    puts e.message, e.class
+  ensure 
+    response
+  end
+
+  def invoke(function_name)
+    response = @lambda_client.invoke({
+      function_name: function_name, 
+      invocation_type: "RequestResponse", 
+      log_type: "Tail", 
+      payload: "",
+    })
+    response
+  end
 end
 
-puts response
+lh = LambdaHandler.new
+puts lh.invoke("a93_message_handler")
