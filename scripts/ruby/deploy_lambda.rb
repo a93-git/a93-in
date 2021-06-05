aa3c1ea1a2e66c2ded3d947710a829e9e1e5855e
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-06 02:19:53 +0530

    Function is zipped and deployed

--- scripts/ruby/deploy_lambda.rb
+++ scripts/ruby/deploy_lambda.rb
@@ -1,36 +1,43 @@
-require 'zip'
-require './generate_file_tree'
+require 'aws-sdk-lambda'
+require './zip_files'
 
-class NoFilesToZip < Exception
-end
+zipper = ZipFilesRec.new(*ARGV[0..-2], ARGV[-1])
+zipper.zip
+zip_filename = zipper.zip_filename
 
-class NoZipFileName < Exception
-end
+Aws.config.update(
+  region: 'us-east-1',
+  credentials: Aws::InstanceProfileCredentials.new
+)
 
-files = ARGV[0..-2]
-zip_filename = ARGV[-1]
-
-raise NoFilesToZip unless files == nil
-raise NoZipFileName unless zip_filename == nil
+lambda_client = Aws::Lambda::Client.new
 
 begin
-  files_to_zip = []
-  files.each do |fn|
-    if File.directory?(fn)
-      files_to_zip += GenerateFileTree.new([]).rec_listing(fn)
-    else
-      files_to_zip << fn
-    end
-  end
-ensure
-  files_to_zip
+  response = lambda_client.create_function({
+    function_name: "a93_message_handler", 
+    runtime: "ruby2.7", 
+    role: "arn:aws:iam::979558485280:role/a93_messaging_lambda_role", 
+    handler: "message_handling_lambda.handler",
+    code: { 
+      zip_file: File.open("#{zip_filename}", "rb"),
+    },
+    description: "Sends SNS notification when a new message ends up in DynamoDB",
+    timeout: 30,
+    memory_size: 128,
+    publish: true,
+    package_type: "Zip"
+  })
+rescue Aws::Lambda::Errors::ResourceConflictException => e
+  puts "Error in creating the Lambda function"
+  puts e.message, e.class
+rescue Aws::Lambda::Errors::AccessDeniedException => e
+  puts "Access denied error while creating the Lambda function"
+  puts e.message, e.class
+rescue Exception => e
+  puts "Error occured while creating the Lambda function"
+  puts e.message, e.class
+ensure 
+  response
 end
 
-files_to_zip.each do |fn|
-  puts "Compressing #{fn}..."
-  Zip::File.open("#{zip_filename}.zip", Zip::File::CREATE) do |zfh|
-    zfh.get_output_stream("#{fn}") do |fh|
-      fh.write(File.open("#{fn}").read)
-    end
-  end
-end
+puts response
