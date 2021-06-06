af2108d0d5e20bb7b6f81dceef66688ef7891cf9
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-06 09:07:58 +0530

    Add list_functions method, invoke the function, list the function

--- scripts/ruby/deploy_lambda.rb
+++ scripts/ruby/deploy_lambda.rb
@@ -55,7 +55,23 @@ class LambdaHandler
     })
     response
   end
+
+  def list_functions
+    response = @lambda_client.list_functions
+  end
+
 end
 
 lh = LambdaHandler.new
-puts lh.invoke("a93_message_handler")
+
+invoke_response = lh.invoke("a93_message_handler")
+puts "Tail log of Lambda invocation:"
+puts Base64.decode64(invoke_response[:log_result])
+
+puts 
+
+
+list_response = lh.list_functions
+list_response[:functions].each do |fun|
+  print "#{fun[:function_name]}\t#{fun[:function_arn]}\n"
+end
