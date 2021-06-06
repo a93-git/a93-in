e4a10ebf886f67a300e91dddd6b801c96195b8b8
a93-git <42908045+a93-git@users.noreply.github.com>
2021-06-06 13:45:06 +0530

    Lambda function to parse the dynamo streams data and send SNS notification

--- scripts/ruby/message_handling_lambda.rb
+++ scripts/ruby/message_handling_lambda.rb
@@ -1,3 +1,37 @@
+require 'aws-sdk-sns'
+
 def handler(event:, context:)
-  puts "Yo"
+  Aws.config.update({
+    "region": "us-east-1"
+  })
+
+  sns_client = Aws::SNS::Client.new 
+  messages = []
+
+  begin
+    event["Records"]&.each do |record|
+      messages.append({
+        "email" => record["dynamodb"]["NewImage"]["email"]&.values,
+        "message" => record["dynamodb"]["NewImage"]["message"]&.values,
+        "id" => record["dynamodb"]["NewImage"]["id"]&.values
+      })
+    end
+  rescue Exception => e
+    puts "Error in parsing the event data"
+    puts e.message, e.class
+  end
+
+  begin
+    response = sns_client.publish({
+      topic_arn: "arn:aws:sns:us-east-1:979558485280:a93-topic",
+      message: messages.to_s, 
+      subject: "You have a new message on #{Time.now.strftime("%Y-%m-%d")}"
+    })
+  rescue Exception => e
+    puts "Error in parsing the vent data"
+    puts e.message, e.class
+  end
+
+  response.message_id
 end
+
